06b1667e15ea720390b873608c5ba8ec13c734ce
srikar1248 <srikar1248@gmail.com>
2018-08-03 10:06:36 +0530

    Add files via upload

---
+++ NotificationsApi/Controllers/NotifyController.cs
@@ -0,0 +1,46 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace NotificationsApi.Controllers
+{
+    public class NotifyController : ApiController
+    {
+        // POST: api/Notification/send
+        [HttpPost]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> Email([FromBody] MailModel model)
+        {
+            string msg = "";
+            try
+            {
+                string requestUrl = ConfigurationManager.AppSettings["RequestUrl"];
+                string content = JsonConvert.SerializeObject(model);
+
+                using (var client = new HttpClient())
+                {
+                    var response = await client.PostAsync(requestUrl, new StringContent(content, Encoding.UTF8, "application/json"));
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        return Request.CreateResponse((HttpStatusCode)200, new { message = "Email has been sent successfully." });
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return Request.CreateResponse((HttpStatusCode)401, new { message = msg });
+        }
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ NotificationsApi/Models/MailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class MailModel
+    {
+        public  string to { get; set; }
+        public string body { get; set; }
+        public string subject { get; set; }
+        public string cc { get; set; }
+    }
+}
